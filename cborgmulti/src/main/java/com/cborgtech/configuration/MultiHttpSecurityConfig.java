ee3a1193d664a4e617d2cf8d152fb67664611c20
seufster <seufster@gmail.com>
2017-08-08 22:39:51 -0400

    fix multi tenant interceptor

--- cborgmulti/src/main/java/com/cborgtech/configuration/MultiHttpSecurityConfig.java
+++ cborgmulti/src/main/java/com/cborgtech/configuration/MultiHttpSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.toasthub.core.general.filter.TenantInterceptor;
 import org.toasthub.security.filter.ToasthubLoginFilter;
 import org.toasthub.security.service.UserManagerSvc;
 
@@ -39,6 +40,7 @@ public class MultiHttpSecurityConfig {
 		
 		protected void configure(HttpSecurity http) throws Exception {
 			http.csrf().disable()
+				.addFilterBefore(tenantInterceptor(), BasicAuthenticationFilter.class)
 				.antMatcher("/api/**")
 				.addFilterAfter(toasthubLoginFilter(), BasicAuthenticationFilter.class)
 				.exceptionHandling()
@@ -77,6 +79,9 @@ public class MultiHttpSecurityConfig {
 		}
 	}
 	
+	private TenantInterceptor tenantInterceptor() {
+		return new TenantInterceptor();
+	}
 	private ToasthubLoginFilter toasthubLoginFilter() {
 		return new ToasthubLoginFilter(userManagerSvc);
 	}
--- cborgmulti/src/main/java/com/cborgtech/configuration/WebMvcConfiguration.java
+++ cborgmulti/src/main/java/com/cborgtech/configuration/WebMvcConfiguration.java
@@ -3,8 +3,7 @@ package com.cborgtech.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.cborgtech.interceptor.TenantInterceptor;
+import org.toasthub.core.interceptor.TenantInterceptor;
 
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
